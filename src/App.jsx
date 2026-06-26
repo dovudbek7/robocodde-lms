e9432c471b77a914eb5519e4debb38c6a75b7567
dovudbek7 <dovudbekm99@gmail.com>
2026-06-26 15:00:37 +0500

    feat: add layout wrapper with sidebar toggle

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import Home from './pages/Home'
+import Lessons from './pages/Lessons'
+import LessonDetail from './pages/LessonDetail'
+import Progress from './pages/Progress'
+import Profile from './pages/Profile'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="courses/:courseId" element={<Lessons />} />
+          <Route path="lessons/:lessonId" element={<LessonDetail />} />
+          <Route path="progress" element={<Progress />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import Topbar from './Topbar'
+import BottomNav from './BottomNav'
+
+export default function Layout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  return (
+    <div className="min-h-screen bg-ios-gray1">
+      {/* Mobile overlay */}
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black/30 z-[190] lg:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
+      )}
+
+      <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+
+      <div className="lg:ml-[240px] flex flex-col min-h-screen">
+        <Topbar onMenuClick={() => setSidebarOpen(true)} />
+        <main className="flex-1 pb-20 lg:pb-8">
+          <Outlet />
+        </main>
+      </div>
+
+      <BottomNav />
+    </div>
+  )
+}
